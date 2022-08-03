57627c7fdc0e09499c72b6f2cdae9c3b0cc8e12c
Daniel David <daniel.10_16@hotmail.com>
2022-08-03 16:31:04 -0500

    editar layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -32,8 +32,12 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
+                    <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('products.index') }}">
+                                    productos
+                                </a>
+                            </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="row justify-content-center">
         <div class="col-4">
-            
+            @include('components.product-card')
         </div>
     </div>
 @endsection
